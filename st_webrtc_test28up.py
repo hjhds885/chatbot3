c93bb670b9d0ef7806f837f889e1bb7444953faa
hjhds885 <151747511+hjhds885@users.noreply.github.com>
2024-12-06 08:03:25 +0900

    Update st_webrtc_test28up.py
    原因調査
    st.write("text_input:",text_input)
                            #これ以降は、音声入力、テキスト入力共通の処理へ
                            #qa(text_input,webrtc_ctx,cap_title,cap_image)

--- st_webrtc_test28up.py
+++ st_webrtc_test28up.py
@@ -479,9 +479,9 @@ def app_sst_with_video():
                     if len(st.session_state.user_input) > 4 :
                         print("st.session_state.user_input=",st.session_state.user_input)  
                         text_input = st.session_state.user_input
-                        
+                        st.write("text_input:",text_input)
                         #これ以降は、音声入力、テキスト入力共通の処理へ
-                        qa(text_input,webrtc_ctx,cap_title,cap_image)
+                        #qa(text_input,webrtc_ctx,cap_title,cap_image)
                         st.write(f"🤖何か話して!")
                         #status_indicator.write("🤖何か話して!")  
                         text_input = ""       
